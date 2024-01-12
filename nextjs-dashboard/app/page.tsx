379afc617e50d26cf314ca3a9c39487f298d3a8a
ayodeji <ayodejiakintobi1@gmail.com>
2024-01-12 12:28:41 +0100

    UPDATE: FULL PROJECTS DISPLAYED

--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import { luistana } from '@/app/ui/fonts';
 import Image from 'next/image';
 
+
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
