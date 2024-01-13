53f6e8c1227b3d35c8f0093e63b5a89b66f3114b
ayodeji <ayodejiakintobi1@gmail.com>
2024-01-13 21:48:42 +0100

    UPDATE: SKELETON ADDED

--- nextjs-dashboard/app/dashboard/loading.jsx
+++ nextjs-dashboard/app/dashboard/loading.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import DashboardSkeleton from '@/app/ui/skeletons';
 
 const loading = () => {
   return (
     <div>
       Loading..
+      <DashboardSkeleton />
     </div>
   )
 }
