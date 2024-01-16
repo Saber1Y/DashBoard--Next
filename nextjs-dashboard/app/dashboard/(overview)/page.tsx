1a0316c1f8d372bd5f8b7f77d7f47c98a5e46f50
ayodeji <ayodejiakintobi1@gmail.com>
2024-01-16 23:04:40 +0100

    updade: streaming

--- nextjs-dashboard/app/dashboard/(overview)/page.tsx
+++ nextjs-dashboard/app/dashboard/(overview)/page.tsx
@@ -1,9 +1,10 @@
 import { Card } from '@/app/ui/dashboard/cards';
+import CardWrapper from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import {  fetchCardData } from '@/app/lib/data'
 import { Suspense } from 'react';
-import { RevenueChartSkeleton, LatestInvoicesSkeleton } from '@/app/ui/skeletons';
+import { RevenueChartSkeleton, LatestInvoicesSkeleton, CardsSkeleton } from '@/app/ui/skeletons';
 import { revenue } from '@/app/lib/placeholder-data';
  
 export default async function Page() {
@@ -36,6 +37,9 @@ export default async function Page() {
         </Suspense>
         <Suspense fallback={<LatestInvoicesSkeleton />}>
           <LatestInvoices  />
+        </Suspense>
+        <Suspense fallback={<CardsSkeleton />}>
+          <CardWrapper />
         </Suspense>
       </div>
     </main>
